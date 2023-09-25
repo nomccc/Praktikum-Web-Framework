71b103c6b03d326560edc77698720781cfe05df9
nomccc <nuravivah224@gmail.com>
2023-09-25 14:55:02 +0800

    add: first commit

---
+++ postest_1/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class>
+        <div class=" grid grid-cols-2 gap-x-10 text-center font-serif items-center">
+            <div >
+                {{-- <img src="/assets/Skincare spa products set for beauty of skin and hair.jpg" alt=""> --}}
+                <img class="rounded-full" src="/assets/20230925095859_[fpdl.in]_mockup-perfume-skin-care-bottle-cosmetic-tube-beauty-makeup-facial-treatment-cleanser-face-foam-beauty-healthcare-branding-packaging_42100-2585_large.jpg" alt="">
+            </div>
+            <div class="px-8">
+                <p class="text-end py-5">-Review Website</p>
+                <h1 class="font-bold text-6xl text-amber-600">Review Product Skincare</h1>
+                <p class=" text-xl py-5 text-gray-500">Ketahui lebih dalam tentang skincare yang akan kamu beli lewat website ini! </p>
+            </div>
+          </div>
+    </div>
+@endsection
